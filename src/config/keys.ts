51d61901252673a1136d2f20c8a5e9f9fec54fd1
Jesús Bermúdez <jema0622@outlook.com>
2021-04-07 16:29:20 -0500

    Depurado de carpetas para implementacion de libreria de control de bases de datos

--- src/config/keys.ts
+++
@@ -1,6 +0,0 @@
-export const keys:any = {
-    host:process.env.HOST || 'localhost',
-    user:process.env.USER || '',
-    password:process.env.PASSWORD|| '',
-    database:process.env.DATABASE || ''
-}
