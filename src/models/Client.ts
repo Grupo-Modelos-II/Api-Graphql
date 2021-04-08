026941bc0937d083aac0d40e9e14b63efd3d800a
Jesus Lozada <jeallomo@hotmail.com>
2021-04-07 20:55:35 -0500

    Modelo cliente

--- src/models/Client.ts
+++ src/models/Client.ts
@@ -1,9 +1,11 @@
 export default class Client{
 
-    private nombre:string = '';
+    private nombre: string = '';
+    private apellido: string = '';
     private id: number | string = 0;
+    private edad: number = 0;
 
-    constructor(id: number | string) {
+    constructor(id: number | string, nombre: string, apellido: string, edad: number) {
         this.id = id;
     }
 
@@ -11,7 +13,15 @@ export default class Client{
         return this.id;
     }
 
-    public getNombre():string{
+    public getNombre():string {
         return this.nombre;
+    }
+
+    public getApellido():string {
+        return this.apellido;
+    }
+
+    public getEdad():number {
+        return this.getEdad;
     }
 }
