bbdf72999bd5a9cb9dda8401435197bb1d623ac1
Jesús Bermúdez <jema0622@outlook.com>
2021-04-08 01:06:51 -0500

    Añadido enum para manejar con mayor facilidad cambio entre bases de datos

--- src/GraphQL/resolvers.ts
+++ src/GraphQL/resolvers.ts
@@ -1,54 +1,41 @@
 import ClientDao from '../models/ClientDao';
 
-const client: ClientDao = new ClientDao();
+import { setDatabaseType, getDatabaseType } from '../database/config/databaseConfig';
+import Client from '../models/Client';
+
+let client: ClientDao = new ClientDao();
 
 
 export const resolvers: { [x: string]: { [x: string]: (_: any, __: any) => any }; } = {
     Query: {
-        Client: async (_: any, {id}: {id: number | string}): Promise<any> => {
+        Client: async (_: any, {id}: {id: number | string}): Promise<Client> => {
             return await client.getClient(id);
         },
-        Clients: async (): Promise<any[]> => {
+        Clients: async (): Promise<Client[]> => {
             return await client.getClients();
         },
 
         DatabaseClient: (): string => {
-            return client.getConnection();
+            return getDatabaseType();
         }
     },
 
     Mutation: {
-        CreateClient: async (_: any, {input}: {input: { [x: string]: string | number }}): Promise<any> => {
+        CreateClient: async (_: any, {input}: {input: { [x: string]: string | number }}): Promise<Client> => {
             
             return await client.createClients(input);
         },
-        UpdateClient: async (_: any, {input}: {input: { [x: string]: string | number }}): Promise<any> => {
+        UpdateClient: async (_: any, {input}: {input: { [x: string]: string | number }}): Promise<Client> => {
             return await client.editClient(input);
         },
-        DeleteClient: async (_: any, {id}: {id: number | string }): Promise<any> => {
+        DeleteClient: async (_: any, {id}: {id: number | string }): Promise<Client> => {
             return await client.deleteClients(id);
         },
 
-        DatabaseClient: (_: any, { name }: {name: string}): boolean => {
-            switch(name) {
-                case 'postgres':
-                case 'postgresql':
-                    client.setPostgresqlDB();
-                    return true;
-
-                case 'mongo':
-                case 'mongodb':
-                    client.setMongoDB();
-                    return true;
-    
-                case 'mariadb':
-                case 'mysql':
-                    client.setMysqlDB();
-                    return true;
-                
-                default:
-                    return false;
-            }
+        DatabaseClient: (_: any, { name }: {name: string}): string => {
+            setDatabaseType(name);
+            client = new ClientDao();
+            return getDatabaseType();
         }
     }
 };
