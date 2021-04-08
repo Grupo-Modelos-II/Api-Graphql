adf3bba1b37e3b29aa86dcbb4bcfb76ca06437f9
Jesús Bermúdez <jema0622@outlook.com>
2021-04-08 01:09:58 -0500

    Se cambian los parametros del constructor de Client y se implementa un constructor más apropiado para ClienDao

--- src/models/Client.ts
+++ src/models/Client.ts
@@ -1,11 +1,11 @@
-export default class Client{
+export default class Client {
 
     private nombre: string = '';
     private apellido: string = '';
     private id: number | string = 0;
     private edad: number = 0;
 
-    constructor(id: number | string, nombre: string, apellido: string, edad: number) {
+    constructor({id, nombre, apellido, edad}: {id: number | string, nombre: string, apellido: string, edad: number}) {
         this.id = id;
         this.nombre = nombre;
         this.apellido = apellido;
@@ -25,6 +25,6 @@ export default class Client{
     }
 
     public getEdad():number {
-        return this.getEdad;
+        return this.edad;
     }
 }
--- src/models/ClientDao.ts
+++ src/models/ClientDao.ts
@@ -1,61 +1,70 @@
+import Client from './Client';
+
 import ConnectionDatabase from "../database/clients/Connection";
 import Mongo from "../database/clients/MongoConnection";
 import Mysql from "../database/clients/MysqlConnection";
 import Postgresql from "../database/clients/PostgresqlConnection";
 
+import { getDatabaseType } from '../database/config/databaseConfig';
+
 export default class ClientDao {
 	private connection!: ConnectionDatabase;
 
 	constructor(){
-		this.setMongoDB();
+		switch(getDatabaseType()) {
+			case 'postgres':
+			case 'postgresql':
+				this.setPostgresqlDB();
+				break;
+
+			case 'mongo':
+			case 'mongodb':
+				this.setMongoDB();
+				break;
+
+			case 'mariadb':
+			case 'mysql':
+			default:
+				this.setMysqlDB();
+		}
 	}
 
 	private newConnection(db: ConnectionDatabase) {
 		db.connect();
 	}
 
-	public setMongoDB(): void {
+	private setMongoDB(): void {
 		this.connection = new Mongo();
 		this.newConnection(this.connection);
 	}
 
-	public setMysqlDB(): void {
+	private setMysqlDB(): void {
 		this.connection = new Mysql();
 		this.newConnection(this.connection);
 	}
 
-	public setPostgresqlDB(): void {
+	private setPostgresqlDB(): void {
 		this.connection = new Postgresql();
 		this.newConnection(this.connection);
 	}
 
-	public getConnection(): string {
-		if(this.connection instanceof Mongo) {
-			return "Mongo"
-		} else if(this.connection instanceof Mysql) {
-			return "Mysql"
-		} else {
-			return "Postgres"
-		}
-	}
-
-	public async createClients(data: any): Promise<any> {
+	public async createClients(data: any): Promise<Client> {
 		return await this.connection.create("Cliente",data);
 	}
 
-	public async getClient(idClient: number|string): Promise<any> {
+	public async getClient(idClient: number|string): Promise<Client> {
 		return await this.connection.get("Cliente",idClient);
 	}
 
-	public async getClients(): Promise<any> {
+	public async getClients(): Promise<Client[]> {
 		return await this.connection.getAll("Cliente");
 	}
 
-	public async editClient(data: any): Promise<any> {
-		return await this.connection.create("Cliente",data);
+	public async editClient(data: any): Promise<Client> {
+		return await this.connection.update("Cliente",data);
 	}
 
-	public async deleteClients(idClient: number|string): Promise<any> {
+	public async deleteClients(idClient: number|string): Promise<Client> {
 		return await this.connection.delete("Cliente",idClient);
 	}
 }
